98ee27f3c92e679d759916c5d5fe0750e5f2c718
Toqeernaqvi <toqeernaqvi66@gmail.com>
2023-03-12 19:07:09 +0500

    fix email work

--- contact_response.php
+++ contact_response.php
@@ -1,21 +1,25 @@
 <?PHP
-$top = "<!DOCTYPE HTML PUBLIC ";
-$bottom = "<body></body></html>";
-$blank = ' ';
-$sep = "\r\n";
+$headers  = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
 $subject = $_POST['subject'];
 $name = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['message'];
-// $contact = $_POST['email'] .$blank .$_POST['phone'];
-$body = $name .$sep .$email .$sep .$phone .$sep .$subject .$sep .$message;
-$headers = "From: $name <$email>\r\n";
-$headers .= "Content-type: text/html\r\n";
-mail('toqeernaqvi66@gmail.com',$subject, $body, $headers);
-	echo ("Thank you for contacting us, " .$name."<br> Your message: <b>");
-	echo $subject;
-	echo "</b>, has been recieved</br> A response will be sent to you in a timely fashion. <br><br> Have a wonderful day! <br> -ASDPS";
+$msg = $_POST['message'];
+$message = "
+Email From : $email
+Subject : $subject
+Message: $msg
+";
+$headers = "From: $email\r\n";
+$headers .= "X-Priority: 3\r\n";
+$headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
 
+if (mail('toqeernaqvi66@gmail.com',$subject, $message, $headers)){
+	echo "<script>alert('Mail was sent !');</script>";
+	echo "<script>document.location.href='contact.php'</script>";
+	}
+	else
+	{
+	echo "<script>alert('Mail was not sent. Please try again later');</script>";
+	}
 ?>
-
-
